5f756cb5eed8bd2529c665df77d75ef316c862ef
callum leung <cleung@scottlogic.com>
2020-02-05 10:20:17 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { shallow } from './enzyme'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('sum two numbers', () => {
+  const wrapper = shallow(<App />);
+  
+  expect(wrapper.find('#firstValue').exists()).toBeFalsy();
+})
